e1ad200fdded9d9dac36dbb47d2be118f8e631de
Trugamr <6trugamr9@gmail.com>
2020-03-11 12:13:42 +0530

    added enter and exit animations to user info in header & sign in button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import UserCard from '../user-card/user-card.component'
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 import { selectCurrentUser } from '../../redux/user/user.selectors'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const headerVariants = {
   hidden: {
@@ -23,10 +23,27 @@ const headerVariants = {
   }
 }
 
+const userVariants = {
+  hidden: {
+    opacity: 0,
+    x: 100
+  },
+  show: {
+    opacity: 1,
+    x: 0
+  }
+}
+
 const spring = {
   type: 'spring',
   damping: 10,
   stiffness: 50
+}
+
+const springUser = {
+  type: 'spring',
+  damping: 25,
+  stiffness: 300
 }
 
 const Header = ({ currentUser }) => {
@@ -44,32 +61,53 @@ const Header = ({ currentUser }) => {
           padding={16}
           borderRadius={3}
           background="tint2"
+          overflow="hidden"
         >
           <Pane flex={1} display="flex" alignItems="center">
             <Logo className="logo" />
             <Heading size={600}> Todo</Heading>
           </Pane>
           <Pane display="flex">
-            {currentUser ? (
-              <Pane display="flex">
-                <UserCard
-                  name={currentUser.displayName}
-                  imageURL={currentUser.photoURL}
-                />
-                <Button
-                  onClick={() => auth.signOut()}
-                  appearance="minimal"
-                  paddingX={8}
+            <AnimatePresence exitBeforeEnter initial={false}>
+              {currentUser ? (
+                <motion.div
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  variants={userVariants}
+                  transition={springUser}
+                  key="forAnimationPresence1"
                 >
-                  <Icon icon="log-out" color="muted" />
-                </Button>
-              </Pane>
-            ) : (
-              <Button onClick={signInWithGoogle} appearance="primary">
-                <span>Sign In</span>
-                <Icon icon="log-in" marginLeft={10} />
-              </Button>
-            )}
+                  <Pane display="flex">
+                    <UserCard
+                      name={currentUser.displayName}
+                      imageURL={currentUser.photoURL}
+                    />
+                    <Button
+                      onClick={() => auth.signOut()}
+                      appearance="minimal"
+                      paddingX={8}
+                    >
+                      <Icon icon="log-out" color="muted" />
+                    </Button>
+                  </Pane>
+                </motion.div>
+              ) : (
+                <motion.div
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  transition={springUser}
+                  variants={userVariants}
+                  key="forAnimationPresence2"
+                >
+                  <Button onClick={signInWithGoogle} appearance="primary">
+                    <span>Sign In</span>
+                    <Icon icon="log-in" marginLeft={10} />
+                  </Button>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </Pane>
         </Pane>
       </motion.div>
