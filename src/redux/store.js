9b8b0e85ca76d3bd3d5b829d49618c67c0e7ed0a
Trugamr <6trugamr9@gmail.com>
2020-03-11 16:22:05 +0530

    added state persistance using redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,24 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import logger from 'redux-logger'
 
 import rootReducer from './root.reducer'
 
-const middlewares = [logger]
+const persistConfig = {
+  key: 'user',
+  storage
+}
 
-export default createStore(rootReducer, applyMiddleware(...middlewares))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middlewares = process.env.NODE_ENV === 'development' ? [logger] : []
+
+// export default createStore(rootReducer, applyMiddleware(...middlewares))
+
+export const store = createStore(
+  persistedReducer,
+  applyMiddleware(...middlewares)
+)
+export const persistor = persistStore(store)
