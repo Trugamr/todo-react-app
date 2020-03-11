49f9076c7c9404d3631fd66e5bf17f1428a61768
Trugamr <6trugamr9@gmail.com>
2020-03-11 12:39:14 +0530

    Updates

--- precache-manifest.a09465bff09fcff5a2f1caa1f4293d21.js
+++ precache-manifest.a09465bff09fcff5a2f1caa1f4293d21.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70314182ff651cb351daefffb7e90c2f",
+    "revision": "0a88fb694bea9ee738cb2bdf28e1c81a",
     "url": "/todo-react-app/index.html"
   },
   {
-    "revision": "9eca38608ac4f473f9cc",
-    "url": "/todo-react-app/static/css/main.afa8915f.chunk.css"
+    "revision": "355b67a16de7e52dc9d4",
+    "url": "/todo-react-app/static/css/main.70140e79.chunk.css"
   },
   {
-    "revision": "c20bbba32e0a32cb1dee",
-    "url": "/todo-react-app/static/js/2.be4f6881.chunk.js"
+    "revision": "158c3c77aada379ccbf2",
+    "url": "/todo-react-app/static/js/2.dc4cc162.chunk.js"
   },
   {
     "revision": "91892fb6c3e2bada3e7a43b7a8b15a5d",
-    "url": "/todo-react-app/static/js/2.be4f6881.chunk.js.LICENSE.txt"
+    "url": "/todo-react-app/static/js/2.dc4cc162.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9eca38608ac4f473f9cc",
-    "url": "/todo-react-app/static/js/main.1ce06469.chunk.js"
+    "revision": "355b67a16de7e52dc9d4",
+    "url": "/todo-react-app/static/js/main.6dfc8a14.chunk.js"
   },
   {
     "revision": "d72ce437e01b72e02cf2",
